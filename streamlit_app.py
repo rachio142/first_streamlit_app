86d9b63d12913562a8fe0f9b517094ff10910917
rachio142 <111333904+rachio142@users.noreply.github.com>
2023-04-25 14:28:31 -0600

    Update streamlit_app.py
    Added text box so users can suggest what fruit to add.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_aadded = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding', fruit_added, '.')
